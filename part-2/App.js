23d5a73bb8d39d42d2ed444ca12fe8de5a6d74d5
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-06 15:31:53 -0400

    Update App.js

--- part-2/App.js
+++ part-2/App.js
@@ -1,27 +1,8 @@
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+    
+    </div>
   );
 }
 
-
-    // <div>
-    //   <Tweet
-    //     name="Matt Lane"
-    //     username="mmmaaatttttt"
-    //     date={new Date().toDateString()}
-    //     message="This app will disrupt everything!!"
-    //   />
-    //   <Tweet
-    //     name="Elie Schoppik"
-    //     username="eschoppik"
-    //     date={new Date().toDateString()}
-    //     message="#Ilovehashtags"
-    //   />
-    //   <Tweet
-    //     name="Tim Garcia"
-    //     username="TimGarcia0"
-    //     date={new Date().toDateString()}
-    //     message="Follow me on Twitter!"
-    //   />
-    // </div>
